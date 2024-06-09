b9ac15698d8441f1255e11da874f88a4bf93a4f2
Rifqi2007c <158813804+Rifqi2007c@users.noreply.github.com>
2024-06-09 21:30:10 +0800

    Add files via upload

---
+++ themes/bspwm_pac/scripts/powermenu.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+rofi_cmd() {
+	rofi -dmenu \
+		-theme ~/.config/bspwm/themes/bspwm_pac/rofi/powermenu.rasi
+}
+
+chosen=$(printf "⏻Shutdown\n󰜉Reboot\n󰗼Logout" | rofi_cmd)
+
+case "$chosen" in
+
+"⏻Shutdown") poweroff ;;
+"󰜉Reboot") reboot ;;
+"󰗼Logout") bspc quit ;;
+*) exit 1 ;;
+
+esac
---
+++ themes/bspwm_pac/scripts/wallmenu.sh
@@ -0,0 +1,25 @@
+cacheDir="$HOME/wallcache"
+wall_dir="$HOME/wallpaper"
+
+if [ ! -d "${cacheDir}" ]; then
+	mkdir -p "${cacheDir}"
+fi
+
+# Convert images in directory and save to cache dir
+for imagen in "$wall_dir"/*.{jpg,jpeg,png,webp}; do
+	if [ -f "$imagen" ]; then
+		nombre_archivo=$(basename "$imagen")
+		if [ ! -f "${cacheDir}/${nombre_archivo}" ]; then
+			convert -strip "$imagen" -thumbnail 500x500^ -gravity center -extent 500x500 "${cacheDir}/${nombre_archivo}"
+		fi
+	fi
+done
+
+rofi_command="rofi -dmenu -theme $HOME/scripts/WallSelect.rasi -theme-str ${rofi_override}"
+
+wall_selection=$(find "${wall_dir}" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) -exec basename {} \; | sort | while read -r A; do echo -en "$A\x00icon\x1f""${cacheDir}"/"$A\n"; done | $rofi_command)
+
+# Set wallpaper
+[[ -n "$wall_selection" ]] || exit 1
+feh --no-fehbg --bg-fill "${wall_dir}"/"${wall_selection}"
+exit 0
